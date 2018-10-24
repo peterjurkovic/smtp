407394a2aeedcbade277f9329b122513a893eaf0
Peter Jurkovic <petojurkovic@gmail.com>
2018-10-24 10:57:26 +0100

    Parsing email

--- src/main/java/com/nexmo/smtp/MmsMessageHandlerFactory.java
+++ src/main/java/com/nexmo/smtp/MmsMessageHandlerFactory.java
@@ -1,14 +1,21 @@
 package com.nexmo.smtp;
 
 import org.apache.commons.io.IOUtils;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.subethamail.smtp.MessageContext;
 import org.subethamail.smtp.MessageHandler;
 import org.subethamail.smtp.MessageHandlerFactory;
 import org.subethamail.smtp.RejectException;
 import org.subethamail.smtp.TooMuchDataException;
 
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.StringWriter;
+import java.util.Enumeration;
+import java.util.Properties;
 
 public class MmsMessageHandlerFactory implements MessageHandlerFactory {
 
@@ -37,10 +44,23 @@ public class MmsMessageHandlerFactory implements MessageHandlerFactory {
 
         @Override
         public void data(InputStream data) throws RejectException, TooMuchDataException, IOException {
-            System.out.println("MAIL DATA");
-            System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
-            System.out.println(this.convertStreamToString(data));
-            System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+            Session session = Session.getInstance(new Properties());
+            try {
+                MimeMessage message = new MimeMessage(session, data);
+                Enumeration en = message.getAllHeaderLines();
+                while (en.hasMoreElements()){
+                    System.out.println(en.nextElement());
+                }
+                System.out.println("Data content type: "+message.getDataHandler().getContentType());
+
+            } catch (MessagingException e) {
+                e.printStackTrace();
+            }
+
+//            System.out.println("MAIL DATA");
+//            System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
+//            System.out.println(this.convertStreamToString(data));
+//            System.out.println("= = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =");
         }
 
         @Override
